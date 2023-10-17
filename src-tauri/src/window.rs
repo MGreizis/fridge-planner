7d5f7454785b91fb6cd6d1e7d68355573732570b
Martins Greizis <grei0003@hz.nl>
2023-10-17 22:25:30 +0200

    starting over because i chose svelte instead of sveltekit lol

--- src-tauri/src/window.rs
+++
@@ -1,8 +0,0 @@
-use tauri::WindowBuilder;
-
-#[tauri::command]
-pub async fn create_window(app: tauri::AppHandle) {
-  let _window = tauri::WindowBuilder::new(&app, "label", tauri::WindowUrl::External("https://tauri.app/".parse().unwrap()))
-    .build()
-    .unwrap();
-}
